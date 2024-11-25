df113a910a06e9c5d8b47cd5743683891c487b9a
zakariabanid101 <157104843+zakariabanid101@users.noreply.github.com>
2024-11-25 10:57:17 +0100

    first commit

---
+++ src/test/java/com/example/autentication_application/AutenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autentication_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
