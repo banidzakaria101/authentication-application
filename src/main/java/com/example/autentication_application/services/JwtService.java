97dac8c262771ebf6772d60fc1389d238f456899
zakariabanid101 <157104843+zakariabanid101@users.noreply.github.com>
2024-11-25 14:22:14 +0100

    add functions to sheck token's validation

--- src/main/java/com/example/autentication_application/services/JwtService.java
+++ src/main/java/com/example/autentication_application/services/JwtService.java
@@ -73,5 +73,16 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername())) &&  !isTokenExpired(token);
+    }
 
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaims(token, Claims::getExpiration);
+    }
 }
